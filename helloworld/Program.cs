df3550833d1156813a62ac751cef9b721519b9ee
zhiqiang.mao <maozhiqiangzbm@126.com>
2016-08-22 15:37:59 +0800

    提交helloworld

---
+++ helloworld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[] { 1, 2, 3, 4, 5 };
+            Console.WriteLine("hello world");
+            String name = Console.ReadLine();
+            Console.WriteLine("{0},{0}",name,name);
+            Console.ReadKey();
+        }
+    }
+}
